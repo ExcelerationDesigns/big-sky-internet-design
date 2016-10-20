ed6b104ed87e66555d31acc08d61ae238f53acc3
Kristen Ducey <Kristen Ducey>
2016-10-20 13:58:35 -0600

    Updates map

--- js/map.js
+++ js/map.js
@@ -11,7 +11,7 @@ function init() {
         zoom: 16,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.722330, -73.992905), // You Address Here
+        center: new google.maps.LatLng(30.706546, -96.295947), // You Address Here
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -33,7 +33,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/misc/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.722330, -73.992905); // You Address Here
+    var myLatLng = new google.maps.LatLng(30.706546, -96.295947); // You Address Here
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
